dfa79c517c5c7bf0ae92739afb35b2f6f0fbfbd0
Sophia Ritchie <sophritchie@gmail.com>
2020-10-02 01:50:41 +1300

    Set up AWS amplify + auth + test graphql queries

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,34 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import AppBar from "@material-ui/core/AppBar";
 import { Container, Typography, Toolbar, IconButton } from "@material-ui/core";
 import { createStyles, makeStyles, Theme } from "@material-ui/core/styles";
 import MoreVertIcon from "@material-ui/icons/MoreVert";
 import SupervisorAccountIcon from "@material-ui/icons/SupervisorAccount";
 import { ThemeProvider } from "./ThemeProvider";
+import { API, graphqlOperation } from 'aws-amplify';
+import { GetTaskQuery } from './API';
+import { listTasks } from './graphql/queries';
+import { withAuthenticator } from '@aws-amplify/ui-react';
+
+const initialState = { name: '', description: '' }
 
 function App() {
+  const [tasks, setTasks] = useState([])
+
+  useEffect(() => {
+    fetchTasks()
+  }, [])
+
+  async function fetchTasks() {
+    try {
+      const taskData: any = await API.graphql(graphqlOperation(listTasks))
+      const tasks = taskData.data.listTasks.items;
+      setTasks(tasks)
+    } catch (err) { console.log('error fetching tasks') }
+  }
+
   const classes = useStyles();
+
   return (
     <ThemeProvider>
       <AppBar>
@@ -23,7 +44,19 @@ function App() {
           </IconButton>
         </Toolbar>
       </AppBar>
-      <Container maxWidth="sm">{}</Container>
+      <Container maxWidth="sm">{}
+        <div>
+          <h2>Amplify Tasks</h2>      {
+            tasks.map((item: any, index) => (
+              <div key={item.id ? item.id : index}>
+                <p>{item.name}</p>
+                <p>{item.description}</p>
+              </div>
+            ))
+          }
+        </div>
+
+      </Container>
     </ThemeProvider>
   );
 }
@@ -36,4 +69,4 @@ const useStyles = makeStyles((theme: Theme) =>
   })
 );
 
-export default App;
+export default withAuthenticator(App)
