107e5b4eb84ed072c41b72572c13c9919d902750
Sophia Ritchie <sophritchie@gmail.com>
2020-10-12 16:06:03 +1300

    Style sign in

--- src/index.tsx
+++ src/index.tsx
@@ -4,7 +4,6 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import Amplify from 'aws-amplify';
-
 import awsExports from "./aws-exports";
 Amplify.configure(awsExports);
 
