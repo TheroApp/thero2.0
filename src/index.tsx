f6ab166c49fee1cdd726db282b078bddaec1220b
Sophia Ritchie <sophiaritchie@Sophias-MacBook-Pro.local>
2020-11-21 16:00:59 +1300

    Remove from index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -9,36 +9,6 @@ Amplify.configure(awsExports);
 
 ReactDOM.render(
   <React.StrictMode>
-    <meta name="apple-mobile-web-app-capable" content="yes"></meta>
-    <meta name="mobile-web-app-capable" content="yes"></meta>
-    <link rel="manifest" href="/manifest.webmanifest"></link>
-    <link //iPhone splashscreens
-      rel="apple-touch-startup-image"
-      href="images/splash/iPhone SE - 1.png"
-      media="(device-width: 320px) and (device-height: 568px) and (-webkit-device-pixel-ratio: 2) and (orientation: portrait)"
-    ></link>
-    <link
-      rel="apple-touch-startup-image"
-      href="images/splash/iPhone 8 - 1.png"
-      media="(device-width: 375px) and (device-height: 667px) and (-webkit-device-pixel-ratio: 2) and (orientation: portrait)"
-    ></link>
-    <link
-      rel="apple-touch-startup-image"
-      href="images/splash/iPhone 8 Plus - 1.png"
-      media="(device-width: 414px) and (device-height: 736px) and (-webkit-device-pixel-ratio: 3) and (orientation: portrait)"
-    ></link>
-    <link
-      rel="apple-touch-startup-image"
-      href="images/splash/X - 1.png"
-      media="(device-width: 375px) and (device-height: 812px) and (-webkit-device-pixel-ratio: 3) and (orientation: portrait)"
-    ></link>
-
-    <link
-      rel="apple-touch-startup-image"
-      href="images/splash/iPhone 11 Pro Max - 4.png"
-      media="(device-width: 414px) and (device-height: 896px) and (-webkit-device-pixel-ratio: 3) and (orientation: portrait)"
-    ></link>
-
     <App />
   </React.StrictMode>,
   document.getElementById("root")
